3b595c9e895e0a3bd1d51b34ff3679df71539b18
Santiago Harkes <santiagoharkes@gmail.com>
2020-11-27 11:27:04 -0300

    Terminamo el jueguitooooooo

--- Clase30/index.js
+++ Clase30/index.js
@@ -1,9 +1,17 @@
 const cartas = document.querySelectorAll(".memoria-card");
+console.log(cartas);
 
+let tableroBloqueado = false;
 let arrancaste = false;
 let primeraCarta, segundaCarta;
 
 function girar() {
+  if (tableroBloqueado) return;
+
+  // Si la primera carta, es igual a la que estoy apretando (segunda), que salga de la funcion
+  // Puedo saber cual es la segunda carta aunque todavía no la haya guardado
+  if (this === primeraCarta) return;
+
   this.classList.add("girar");
 
   if (!arrancaste) {
@@ -19,6 +27,15 @@ function girar() {
   comprobarCoincidencia();
 }
 
+// Funcion para reseTEAR LAS BAREAVLES
+
+function resetearBareavle() {
+  primeraCarta = null;
+  segundaCarta = null;
+  // tableroBloqueado = false;
+  // arrancaste = false;
+}
+
 function comprobarCoincidencia() {
   let coincidencia =
     primeraCarta.dataset.participante === segundaCarta.dataset.participante;
@@ -29,20 +46,51 @@ function comprobarCoincidencia() {
 function deshabilitarCartas() {
   primeraCarta.removeEventListener("click", girar);
   segundaCarta.removeEventListener("click", girar);
+  resetearBareavle();
 }
 
 function devolverCartas() {
+  tableroBloqueado = true;
   setTimeout(() => {
     primeraCarta.classList.remove("girar");
     segundaCarta.classList.remove("girar");
+    tableroBloqueado = false;
+    resetearBareavle();
   }, 700);
 }
 
 function cartasClickeables() {
   cartas.forEach((carta) => carta.addEventListener("click", girar));
+  mezclarCartas();
 }
 
 cartasClickeables();
 
-// Corregir Bug
 // Hacer el random
+
+function mezclarCartas() {
+  // Recorrer todas las cartas
+  // Por cada carta:
+  // Quiero generar un numero random
+  // Y aplicarselo al style.order
+
+  cartas.forEach((carta) => {
+    let numeritoRandomico = Math.floor(Math.random() * 12);
+    carta.style.order = numeritoRandomico;
+  });
+}
+
+// TAREA
+// leer que es this en los addEventListener
+// Agregarle algun boton de Reset (dar vuelta todas las cartas, volver a mezclarlas, volver a agregarle los event listener... todo de nuevo)
+// Contador de movimientos || un limite de movimientos (maximo de 12 movimientos y por cada !acierto, que reste uno, y cuando llega a 0, chau picho)
+// Algun innerHtml que me diga: ganasTE PERRRIIIIIIIIIIII
+
+// Sacado de contexto (miren la clase para entender xD)
+// piedra = 0
+// papel = 2
+// tijera = 1
+
+// const array = [[empato, gano, perdio], [gano, empato, perdio], [perdio, gano, empato]]
+
+// array[0][1] = gano
